5ad3c06da3c0eff0ab45f9e50e03cb36ff71d54f
root <root@DESKTOP-JALIH79.localdomain>
2022-10-05 16:46:35 -0500

    10/5 Meeting Setup

--- dataCollection.py
+++ dataCollection.py
@@ -5,11 +5,12 @@ import csv
 def openInputStream():
     string = input("Enter Turn: ")
     list = []
+
     return list
 
 # saves a list formatted from above into a .csv
 # num is the current game number for file naming
-def closeInputStream(list, num, location = ""):
+def save(list, num, location = ""):
     outputFile = open("fileName.csv", 'w')
 
 
@@ -17,17 +18,29 @@ def closeInputStream(list, num, location = ""):
 
 # saves all games onto the end of allData
 # each game ends with an empty line
+# each game starts with the file name it came from without the csv alone on a line
 # num is number of games
-def compoundFiles(num):
+def saveAll(num):
     mainFile = open("allData.csv", 'a')
 
 
     mainFile.close()
 
-#returns 1 if p1 wins, 2 if p1 loses, and 0 if tie
+# delete specific RPS game from mainData
+def delete(num, location):
+    return False
+
+# delete all files up in location
+def deleteFiles(num, location):
+    return False
+
+#returns 1 if p1 wins, 2 if p2 wins, and 0 if tie
 def rpsOutcome(p1, p2):
     return 0
 
 
-
-currGame = 0
+# write code that will on run openInputStream
+# save input after each game
+# prompt user to stop after each openInputStream
+# on stop saveAll
+# after saving to main file, delete extra csv files created
